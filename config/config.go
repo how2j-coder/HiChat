4a6906e7324329c167cb73f8a3ad91d07ed1f024
BSY002 <bsy002@bsy.com>
2025-02-26 16:33:06 +0800

    feat: del all & re

--- config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type ServiceConfig struct {
-	DB   dbSqlConfig `mapstructure:"mysql" json:"mysql"`
-	RedisDB RedisConfig `mapstructure:"redis" json:"redis"`
-	Port int         `mapstructure:"port" json:"port"`
-	Log  Log         `mapstructure:"log" json:"log"`
-}
