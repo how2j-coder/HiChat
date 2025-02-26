902c173f2acd043aa69d50032932e8a9ad4fcd3b
BSY002 <bsy002@bsy.com>
2025-02-26 18:02:51 +0800

    feat: init project...

---
+++ cmd/chat/main.go
@@ -0,0 +1,22 @@
+package chat
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run(":8077")
+	if err != nil {
+		fmt.Println("1231312")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
