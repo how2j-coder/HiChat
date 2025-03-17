163eab662da183cc449521c16f6e1b80042ef683
BSY002 <bsy002@bsy.com>
2025-03-17 17:14:38 +0800

    feat: 添加请求参数效验规则

--- pkg/gin/validator/validator.go
+++ pkg/gin/validator/validator.go
@@ -26,7 +26,7 @@ func NewCustomValidator() *CustomValidator {
 	return &CustomValidator{}
 }
 
-// ValidateStruct Instantiate struct valid
+// ValidateStruct Instantiate struct valid 实例化valid结构
 func (v *CustomValidator) ValidateStruct(obj interface{}) error {
 	if kindOfData(obj) == reflect.Struct {
 		v.lazyInit()
@@ -48,6 +48,10 @@ func (v *CustomValidator) lazyInit() {
 	v.Once.Do(func() {
 		v.Validate = valid.New()
 		v.Validate.SetTagName("binding")
+
+		// 注册自定义校验规则
+		// optional_not_empty 空效验 字段类型要设置为指针类型
+		_ = v.Validate.RegisterValidation("optional_not_empty", validateOptionalNotEmpty, true)
 	})
 }
 
@@ -59,4 +63,24 @@ func kindOfData(data interface{}) reflect.Kind {
 		valueType = value.Elem().Kind()
 	}
 	return valueType
+}
+
+func validateOptionalNotEmpty(fl valid.FieldLevel) bool {
+	// 获取字段的值
+	field := fl.Field()
+	if field.Kind() == reflect.Ptr && field.IsNil() {
+		return true
+	}
+
+	// 如果字段传入了，则检查其值是否为空
+	switch field.Kind() {
+	case reflect.String:
+		return field.String() != ""
+	case reflect.Slice, reflect.Map, reflect.Array:
+		return field.Len() > 0
+	case reflect.Ptr, reflect.Interface:
+		return !field.IsNil()
+	default:
+		return true // 其他类型默认通过
+	}
 }
