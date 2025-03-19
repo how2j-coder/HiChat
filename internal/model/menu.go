f804ac39f1f4cd88e37cf0dbdfe3987c83fe907c
BSY002 <bsy002@bsy.com>
2025-03-19 18:06:49 +0800

    feat: 添加菜单与角色多对多规则

--- internal/model/menu.go
+++ internal/model/menu.go
@@ -5,7 +5,7 @@ import "com/chat/service/pkg/datastore/mysql"
 type Menu struct {
 	mysql.BaseModel `gorm:"embedded"`
 	ParentMenuID    uint64 `gorm:"default:0;comment:父级菜单ID"`
-	MenuName		string `gorm:"type:varchar(20);comment:菜单名称"`
+	MenuName        string `gorm:"type:varchar(20);comment:菜单名称"`
 	MenuCode        string `gorm:"type:varchar(20);unique_index;not null;comment:菜单唯一Code"`
 	MenuPath        string `gorm:"type:varchar(255);not null;comment:菜单对于地址"`
 	MenuSource      string `gorm:"type:varchar(255);not null;comment:菜单对应前端的文件页面地址"`
@@ -13,6 +13,7 @@ type Menu struct {
 	Type            int8   `gorm:"default:1;not null;comment:菜单类型（1：菜单，2：按钮，3：其他）"`
 	IsRefresh       int8   `gorm:"default:1;not null;comment:页面刷新（1：刷新，0：不刷新）"`
 	IsVisible       int8   `gorm:"default:1;comment:是否可见(0 隐藏 1 显示)"`
+	Role            []Role `gorm:"many2many:role_menu"`
 }
 
 func (u *Menu) TableName() string {
